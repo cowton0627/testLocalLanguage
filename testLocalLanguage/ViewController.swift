7b318e9020837a4e6a1c1499e765ed8afd20365b
Charles Cheng <charles_cheng@microjet.com.tw>
2023-06-16 11:48:31 +0800

    Initial Commit

---
+++ testLocalLanguage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testLocalLanguage
+//
+//  Created by Chun-Li Cheng on 2023/6/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
